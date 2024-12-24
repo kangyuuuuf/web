1d82b270fb9a4bb11edd428085bcdfa84ef1ec52
kangyuuuuf <ericfeng0821@gmail.com>
2024-12-24 16:27:19 -0600

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import LandPage from './components/landPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is <code>Kangyu's</code> personal website.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <LandPage />
     </div>
   );
 }
