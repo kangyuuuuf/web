6f0627655304a73375477d5fe5fb1385335f4de3
kangyuuuuf <ericfeng0821@gmail.com>
2025-01-02 22:49:24 -0600

    add ani

--- src/components/navigate/navigate.js
+++ src/components/navigate/navigate.js
@@ -1,24 +1,69 @@
-
 import "./navigate.css";
-import {animate, motion} from 'framer-motion';
-import React, { useState, useEffect, useRef } from 'react';
+import { animate, delay, motion } from "framer-motion";
+import React from "react";
+import { useInView } from "react-intersection-observer";
 
-
-function  Navigate() {
+function Navigate() {
+  const containerVariants = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3, // Controls the delay between each child's animation
+      },
+    },
+  };
+  
+  const itemVariants = {
+    hidden: { opacity: 0, x: 20 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+  };
   const items = [
-    { id: 1, text: "CS 124", info: "Intro to Computer Science I",  image: "/124.png" },
+    { id: 1, text: "CS 124", info: "Intro to Computer Science I", image: "/124.png" },
     { id: 2, text: "CS 357", info: "Numerical Methods I", image: "/357.png" },
-    { id: 3, text: "CS 519", info: "Scientific Visualization", image: "/519.png"},
+    { id: 3, text: "CS 519", info: "Scientific Visualization", image: "/519.png" },
   ];
+  const updates = [
+    { id: 1, time: "2020.08", info: "Started my undergraduate study at UIUC"},
+    { id: 2, time: "2024.05", info: "Graduated with Highest Honors in Computer Science" },
+    { id: 3, time: "2024.06", info: "Joined ANE Logistics as an intern, working on updating API infrastructure" },
+    { id: 4, time: "2024.08", info: "Started MCS program at UIUC" },
+    
+  ];
+
+  const AnimatedElement = ({ children, component: Component = "p", className, ...rest }) => {
+    const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+    const animation = {
+      initial: { opacity: 0, y: 20 },
+      animate: { opacity: 1, y: 0 },
+    };
+  
+    return (
+      <motion.div
+        ref={ref}
+        initial="initial"
+        animate={inView ? "animate" : "initial"}
+        transition={{ duration: 0.8 }}
+        variants={animation}
+      >
+        {/* Pass className and any additional props */}
+        <Component className={className} {...rest}>
+          {children}
+        </Component>
+      </motion.div>
+    );
+  };
+
   return (
-    <motion.div className="container" 
-      initial={{opacity:0}}
-      animate={{opacity:1}}>
+    <motion.div
+      className="container"
+    >
       {/* Sidebar Section */}
-      <div className="sidebar">
+      <motion.div className="sidebar"       initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}>
         <motion.img
           whileHover={{ rotate: 10 }}
-          onClick={() => window.location.href = "/"}
+          onClick={() => (window.location.href = "/")}
           src="/image.png" // Replace with your profile image URL
           className="profile-image"
         />
@@ -33,87 +78,133 @@ function  Navigate() {
           <p>🌐 Website</p>
           <p>📄 Curriculum Vitae</p>
         </div>
-      </div>
+      </motion.div>
 
-      {/* Main Content Section */}
-<div className="main-content">
-
+      <div className="main-content">
       {/* Bio Section */}
       <section className="subsection" id="bio">
-        <h2>Intro</h2>
-        <p>
-          I majored in computer science at the Grainger College of Engineering @ 
+        <AnimatedElement component="h2">Intro</AnimatedElement>
+        <AnimatedElement>
+          I majored in computer science at the Grainger College of Engineering @{" "}
           <motion.a
             href="https://illinois.edu/"
             target="_blank"
             rel="noopener noreferrer"
-            whileHover={{ color: "#0056b3" }} // Change color on hover
-            style={{ display: "inline", color: "#007bff", transition: "color 0.3s ease" }}
-          > UIUC
-          </motion.a> and earned my <strong>Bachelor of Science</strong> degree with Highest Honors in May 2024. 
-          I am currently enrolled in the MCS program as a graduate student starting in Fall 2024, conducting research on utilizing CS tools to improve non-CS elementary courses.
-        </p>
-        <p>
-          My fields of interests are mainly artificial intelligence/machine learning, game development, and <motion.a
-            href="https://publish.illinois.edu/computingedresearch/"
-            target="_blank"
-            rel="noopener noreferrer"
             whileHover={{ color: "#0056b3" }}
             style={{ display: "inline", color: "#007bff", transition: "color 0.3s ease" }}
-          > computer science education
-          </motion.a>.
-        </p>
-      </section>
-
-      {/* Research Interests Section */}
-      <section className="subsection" id="research">
-        <h2>Enthusiasm for Education in Computer Science</h2>
-        <p>
-          Besides learning how fundamental data structures and algorithm’s function are implemented with my professors and mentor, my teaching experience at Grainger College of Engineering also reinforced my commitment to create an inclusive and supportive playground in entertainment technology, from breaking language barriers for international students to weaving programming expertise into the diverse fabric of interdisciplinary research.
-        </p>
-        <p>The following are the courses for which I have been working as an undergraduate course assistant:</p>
-        <div className="container-class">
-          {items.map((item) => (
-            <div key={item.id} className="circle-container">
-              <div className="circle">
-                <img src={item.image} alt={`Circle ${item.id}`} className="circle-image" />
-              </div>
-              <div className="circle-text">{item.text}</div>
-              <div className="circle-text2">{item.info}</div>
-            </div>
-          ))}
-        </div>
-        <p>
-          I am currently working with 
+          >
+            UIUC
+          </motion.a>{" "}
+          and earned my <strong>Bachelor of Science</strong> degree with Highest Honors in May
+          2024. I am currently enrolled in the MCS program as a graduate student starting in Fall
+          2024, continuing my journey in the field of computer science. I have the privilege to work
+          with Prof.{" "}
           <motion.a
             href="https://mfsilva.web.illinois.edu/"
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ color: "#0056b3" }}
             style={{ display: "inline", color: "#007bff", transition: "color 0.3s ease" }}
-          > Dr. Mariana Silva
-          </motion.a>, who advocates 
+          >
+            Mariana Silva
+          </motion.a>{" "}
+          as her research assistant, conducting research on utilizing CS tools to improve non-CS
+          elementary courses.
+        </AnimatedElement>
+        <AnimatedElement>
+          My fields of interest are mainly artificial intelligence/machine learning, game
+          development, and{" "}
           <motion.a
-            href="https://en.wikipedia.org/wiki/Mastery_learning#:~:text=Mastery%20learning%20is%20a%20set%2Cthey%20are%20given%20enough%20time."
+            href="https://publish.illinois.edu/computingedresearch/"
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ color: "#0056b3" }}
             style={{ display: "inline", color: "#007bff", transition: "color 0.3s ease" }}
-          > mastery learning
-          </motion.a>, on the 
+          >
+            computer science education
+          </motion.a>
+          .
+        </AnimatedElement>
+      </section>
+
+      {/* Updates Section */}
+      <section className="subsection" id="updates">
+        <AnimatedElement component="h2">Updates</AnimatedElement>
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          className="timeline-container"
+        >
+          {updates.slice().reverse().map((update) => (
+            <motion.p
+              key={update.id}
+              className="timeline"
+              variants={itemVariants}
+            >
+              <span>>> {update.time}</span>
+              <span style={{ display: "inline-block", width: "4ch" }}></span>
+              <span>{update.info}</span>
+            </motion.p>
+          ))}
+        </motion.div>
+      </section>
+
+      {/* Research Interests Section */}
+      <section className="subsection" id="research">
+        <AnimatedElement component="h2">Education in Computer Science</AnimatedElement>
+        <AnimatedElement>
+          Besides learning how fundamental data structures and algorithms function, my teaching
+          experience at Grainger College of Engineering also reinforced my commitment to creating an
+          inclusive and supportive playground in entertainment technology, from breaking language
+          barriers for international students to weaving programming expertise into the diverse
+          fabric of interdisciplinary research.
+        </AnimatedElement>
+        <AnimatedElement>
+          The following are the courses for which I have been working as an undergraduate course
+          assistant:
+        </AnimatedElement>
+        <AnimatedElement component="div" className="container-class">
+          {items.map((item) => (
+            <div key={item.id} className="circle-container">
+              <motion.div className="circle" whileHover={{ scale: 1.1 }}>
+                <img src={item.image} alt={`Circle ${item.id}`} className="circle-image" />
+              </motion.div>
+              <div className="circle-text">{item.text}</div>
+              <div className="circle-text2">{item.info}</div>
+            </div>
+          ))}
+        </AnimatedElement>
+        <AnimatedElement>
+          I am currently working with my mentor,{" "}
+          <motion.a
+            href="https://mfsilva.web.illinois.edu/"
+            target="_blank"
+            rel="noopener noreferrer"
+            whileHover={{ color: "#0056b3" }}
+            style={{ display: "inline", color: "#007bff", transition: "color 0.3s ease" }}
+          >
+            Dr. Mariana Silva
+          </motion.a>{" "}
+          on the{" "}
           <motion.a
             href="https://ae3.grainger.illinois.edu/holding/grants-for-the-advancement-of-teaching-engineering-gate"
             target="_blank"
             rel="noopener noreferrer"
             whileHover={{ color: "#0056b3" }}
             style={{ display: "inline", color: "#007bff", transition: "color 0.3s ease" }}
-          > GATE
-          </motion.a> project—which was to create a new curriculum for CS 101, a large-scale and introductory course with students from various engineering majors—emphasizing the significance of detecting changes in students’ psychological states and providing appropriate emotional support.
-        </p>
+          >
+            GATE
+          </motion.a>{" "}
+          project—which focuses on creating a new curriculum for CS 101, a large-scale introductory
+          course with students from various engineering majors—emphasizing the significance of
+          detecting changes in students’ psychological states and providing appropriate emotional
+          support.
+        </AnimatedElement>
       </section>
     </div>
     </motion.div>
   );
-};
+}
 
 export default Navigate;
