45f8fc71642dd0aa7b73f4d0621d30f0841ed5c6
kangyuuuuf <ericfeng0821@gmail.com>
2025-01-21 10:48:32 -0600

    uodates

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,37 +1,84 @@
+
+
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './about.css';
 
 const About = () => {
-  const [activeContent, setActiveContent] = useState('Story');
-
+  const [activeContent, setActiveContent] = useState('Education');
+  const [activeNumber, setActiveNumber] = useState(0);
   const content = {
-    Story: (
-      <div className='mid'>
-        <h2>Story</h2>
-        <p>
-        Growing up in Shanghai, I've often felt the weight of my parents' journey. In 2000, they left their hometown, which is a small, impoverished village in Hunan Province, a place that still feels like a distant memory for me. As a first-generation in my family who left my hometown, my father made sacrifices and efforts to provide me with opportunities that our peers back home could only dream of.
-        </p>
-        <p>  
-        As I reflect on my journey, I realize that I'm part of a new chapter for our family - one where we've transcended geographical boundaries and cultural silos. Just as my parents' departure from Hunan Province marked a significant shift in our family's trajectory, my decision to study abroad in the US represents another milestone. As a first-generation international student,  it's also an honor to be part of this journey that began with their courage and perseverance.
+    Education: (
+      <motion.div
+        className="mid"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+      <div className="education-section">
+      <h2 >Education</h2>
+      <div className="education-content">
+        <div className="university">
+          <h3>University of Illinois at Urbana-Champaign</h3>
+          <div id="column-container">
+            <p className="degree">
+              <em>Bachelor of Science: Computer Science in Grainger Engineering</em>
+            </p>
+            <p className="dates">Aug. 2020 - May 2024</p>
+          </div>
+        </div>
+        <p className="gpa">Overall GPA: 3.98/4.0 ｜ Technical GPA: 3.99/4.0</p>
+        <p className="deans-list">
+          <strong>Deans List:</strong> Fall 2020, Spring 2021, Fall 2022, Spring 2022, Fall 2023, Spring 2023
         </p>
       </div>
+      </div>
+      <div className="education-section">
+      <h2 className="section-title"></h2>
+      <div className="education-content">
+        <div className="university">
+          <h3>University of Illinois at Urbana-Champaign</h3>
+          <div id="column-container">
+            <p className="degree">
+              <em>Master of Computer Science</em>
+            </p>
+            <p className="dates">Aug. 2024 - Present</p>
+          </div>
+        </div>
+        <p className="gpa">Overall GPA: 4.0/4.0 </p>
+      </div>
+      </div>
+      </motion.div>
     ),
     Contact: (
-      <div className='mid'>
+      <motion.div
+        className="mid"
+        initial={{ opacity: 0,}}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <h2>Contact</h2>
-        <p>
-          kangyuf2[at]illinois[dot]edu / kangyuuuuf[at]gmail[dot]com
-        </p>
-      </div>
+        <p>kangyuf2[at]illinois[dot]edu / kangyuuuuf[at]gmail[dot]com</p>
+      </motion.div>
     ),
     Donation: (
-      <div className='mid'>
+      <motion.div
+        className="mid"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <h2>Donation</h2>
         <p>
-        As I began receiving my salary as an undergraduate course assistant, I  made a conscious decision to set aside funds specifically to support the  growth and development of children monthly. It's a privilege to contribute to their well-being and provide them with opportunities that will shape their futures. This commitment has been a source of fulfillment for me, and I'm  grateful for the chance to make a positive impact on the world through my actions.
+          As I began receiving my salary as an undergraduate course assistant, I made a conscious
+          decision to set aside funds specifically to support the growth and development of children
+          monthly. It's a privilege to contribute to their well-being and provide them with
+          opportunities that will shape their futures.
         </p>
-      </div>
+      </motion.div>
     ),
   };
 
@@ -44,7 +91,7 @@ const About = () => {
         animate={{ x: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <button onClick={() => setActiveContent('Story')}>Story</button>
+        <button onClick={() => setActiveContent('Education')}>Education</button>
         <button onClick={() => setActiveContent('Contact')}>Contact</button>
         <button onClick={() => setActiveContent('Donation')}>Donation</button>
       </motion.div>
--- src/components/terminal/terminal.js
+++ src/components/terminal/terminal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef,useState, useEffect } from 'react';
 import TypeIt from "typeit-react";
 import './terminal.css';
 import {motion} from 'framer-motion';
@@ -8,6 +8,7 @@ function Terminal() {
   const [currentLog1, setCurrentLog1] = useState('');
   const [currentLog2, setCurrentLog2] = useState('');
   const [currentLog3, setCurrentLog3] = useState('');
+  const typeItRef = useRef(null);
   // Function to update current time
   useEffect(() => {
     const updateTime = () => {
@@ -20,7 +21,14 @@ function Terminal() {
 
     updateTime(); // Set the initial time
     const timer = setInterval(updateTime, 1000); // Update time every second
-    return () => clearInterval(timer); // Cleanup interval on unmount
+    return () => {
+      clearInterval(timer); 
+      if (typeItRef.current) {
+        console.log("what")
+        typeItRef.current.destroy();
+        typeItRef.current = null;
+      }
+    }// Cleanup interval on unmount
   }, []);
   return (
     
@@ -65,6 +73,9 @@ function Terminal() {
                 // this is the ASCII art with some spaces replaced by \u2007
                 strings: ['\u2007_\u2007\u2007\u2007_\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007___\u2007_\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007', '|\u2007|\u2007|\u2007|\u2007___\u2007_\u2007\u2007\u2007_\u2007\u2007\u2007\u2007|_\u2007_(\u2007)_\u2007__\u2007___\u2007\u2007', "|\u2007|_|\u2007|/\u2007_\u2007\\\u2007|\u2007|\u2007|\u2007\u2007\u2007\u2007|\u2007||/|\u2007'_\u2007\u2007\u2007_\u2007\\\u2007", '|\u2007\u2007_\u2007\u2007|\u2007\u2007__/\u2007|_|\u2007|_\u2007\u2007\u2007|\u2007|\u2007\u2007|\u2007|\u2007|\u2007|\u2007|\u2007|', '|_|\u2007|_|\\___|\\__,\u2007(\u2007)\u2007|___|\u2007|_|\u2007|_|\u2007|_|', '|\u2007|/\u2007/__\u2007_\u2007_|___/|/_\u2007_\u2007_\u2007\u2007\u2007_\u2007_\u2007\u2007\u2007_\u2007\u2007-\u2007', "|\u2007'\u2007//\u2007_\u2007\u2007|\u2007'_\u2007\\\u2007/\u2007_\u2007\u2007|\u2007|\u2007|\u2007|\u2007|\u2007|\u2007||\u2007|", '|\u2007.\u2007\\\u2007(_|\u2007|\u2007|\u2007|\u2007|\u2007(_|\u2007|\u2007|_|\u2007|\u2007|_|\u2007||_|', '|_|\\_\\__,_|_|\u2007|_|\\__,\u2007|\\__,\u2007|\\__,_/(_)', '\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007|___/\u2007|___/\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007\u2007']
                 ,
+                beforeStep: function (instance) {
+                  typeItRef.current = instance;
+                },
                 afterComplete: function (instance) {
                 setTimeout(() => {
                     instance.destroy(); // Destroy the instance after 2 seconds
@@ -102,6 +113,9 @@ function Terminal() {
                 loop: true,
                 breakLines: false,
                 nextStringDelay: [5000, 1000],
+                beforeStep: function (instance) {
+                  typeItRef.current = instance;
+                },
                 loopDelay: [1000, 5000],
                 strings: ["I majored in computer science at the Grainger College of Engineering @ UIUC and earned my Bachelor of Science degree with Highest Honors in May 2024.", "I am currently enrolled in the MCS program as a graduate student starting in Fall 2024, conducting research on utilizing CS tools to improve non-CS elementary courses.", "My fields of interest are mainly artificial intelligence/ machine learning, game development, and education in computer science."]
             }}
